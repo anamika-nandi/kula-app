c8f75d3d0f2afdf1a105f2bea1076325ae251363
Anshu Raj <50476777+anshuopinion@users.noreply.github.com>
2024-05-07 19:04:02 +0530

    refactor: Update OnlyDesktop component with Chakra UI styles and positioning

--- src/components/OnlyDesktop.tsx
+++ src/components/OnlyDesktop.tsx
@@ -8,6 +8,7 @@ import {
   VStack,
   Box,
   Stack,
+  Icon,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import { GoArrowUpLeft } from "react-icons/go";
@@ -22,7 +23,7 @@ export function OnlyDesktop(props: OnlyDesktopProps) {
   return (
     <>
       <Box h="100vh" display={{ base: "flex", lg: "none" }}>
-        <Stack>
+        <Stack pos="absolute" top="0" left="0">
           <Box>
             <Image
               src="/img/kula_logo.png"
@@ -31,15 +32,16 @@ export function OnlyDesktop(props: OnlyDesktopProps) {
               alt="kula logo"
             />
           </Box>
-          <Box boxSize="22px">
-            <GoArrowUpLeft />
+          <Box pos="absolute" top="24" left="8">
+            <Icon as={GoArrowUpLeft} boxSize="10" color="white" />
           </Box>
         </Stack>
-        <Center>
+        <Center w="full">
           <HStack display="flex" alignItems="center" justifyContent="center">
             <VStack maxW="335px" w="full" gap="45px">
               <Text
                 textStyle="heading"
+                textColor="white"
                 textAlign="center"
                 lineHeight="120%"
                 fontSize="34px"
@@ -75,9 +77,11 @@ export function OnlyDesktop(props: OnlyDesktopProps) {
           height="full"
           alignItems="flex-end"
           justifyContent="flex-end"
-          boxSize="22px"
+          pos="absolute"
+          bottom="8"
+          right="8"
         >
-          <HiOutlineArrowDownRight />
+          <Icon as={HiOutlineArrowDownRight} boxSize="10" color="white" />
         </Stack>
       </Box>
       <Box display={{ base: "none", lg: "block" }}>{children}</Box>
